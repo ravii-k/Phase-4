4a8937061af94b61b42bdbe83f3c124382976bb8
Ravi <kashyap01237@gmail.com>
2022-03-17 12:24:47 +0530

    signing in

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { QuestionComponent } from './question/question.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+
+const routes: Routes = [
+{path:'',redirectTo:'welcome',pathMatch:"full"},
+{path:"welcome", component:WelcomeComponent},
+{path:"question", component:QuestionComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
